47e4810702ec88557ff668deb20d5ec492dca02f
JustARand0m <korbinian.federholzner@t-online.de>
2019-07-15 14:09:16 +0200

    updated changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { TodosComponent } from './pages/todos/todos.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { FitComponent } from './pages/fit/fit.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'todos', component: TodosComponent},
   {path: 'settings', component: SettingsComponent},
+  {path: 'fit', component: FitComponent},
   {path: '', redirectTo: 'todos', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule} from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { TodosComponent } from './pages/todos/todos.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { FitComponent } from './pages/fit/fit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    SettingsComponent
+    SettingsComponent,
+    FitComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
