e7305eb0de321a94115c8ff36c4b9b4af5b68406
JustARand0m <korbinian.federholzner@t-online.de>
2019-10-10 16:40:33 +0200

    started working on backend for folders for summeries

--- src/app/components/summaries-main/summaries-main.component.ts
+++ src/app/components/summaries-main/summaries-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-summaries-main',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summaries-main.component.scss']
 })
 export class SummariesMainComponent implements OnInit {
+  public name: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.name = this.route.snapshot.paramMap.get('id');
   }
 
 }
